f2154de96a8bcd8b6eea8e0f2092663b8d979922
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-21 09:40:02 +0200

    changed the names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
       <Link to="/" className="logo-link" onClick={handleHomeClick}>
         {/* SVG logo with neon pink theme */}
         <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 50" width="200" height="50">
-  <text x="5" y="35" fontSize="28" fontWeight="bold" fill="#f0c">Radiant</text>
-  <text x="110" y="35" fontSize="28" fontWeight="bold" fill="#f0c">Voices</text>
+  <text x="5" y="35" fontSize="28" fontWeight="bold" fill="#f0c">MAGIC</text>
+  <text x="110" y="35" fontSize="28" fontWeight="bold" fill="#f0c">VOICE</text>
 </svg>
 
       </Link>
